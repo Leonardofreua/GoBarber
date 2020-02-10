2ccef61874c5894734ad6f3c32c5865807fa8d89
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-02-10 16:41:10 -0300

    created user models and migrate

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'test',
+    email: 'test@test.com',
+    password_hash: '123423432432',
+  });
+
+  return res.json(user);
 });
 
 export default routes;

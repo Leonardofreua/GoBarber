b4c56d8fb2eb637d19f1c87e0024842d49980a5c
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-02-13 12:55:41 -0300

    user schedule listing implemented.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,9 +1,35 @@
 import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore } from 'date-fns';
+
 import Appointment from '../models/Appointment';
 import User from '../models/User';
+import File from '../models/File';
 
 class AppointmentController {
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       date: Yup.date().required(),
@@ -25,7 +51,7 @@ class AppointmentController {
 
     if (!checkIsProvider) {
       return res
-        .status(400)
+        .status(401)
         .json({ error: 'You can only create appointments with providers' });
     }
 

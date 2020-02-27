dcec3a86058c6940a195a656ea0ed437c5a075c5
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-02-27 16:45:56 -0300

    Created the configuration for Nodemailer.

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '1ef05fb755d873',
+    pass: 'f6d8f2d1da15c2',
+  },
+  default: {
+    from: 'GoBarber Team <noreply@gobarber.com>',
+  },
+};

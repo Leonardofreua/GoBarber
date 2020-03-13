30cda4e9c781afc58c8f8e7118d9584cded4eb50
Leonardo Freua <leonardo.batista.freua@gmail.com>
2020-03-13 17:20:47 -0300

    Update routes.js

--- src/routes.js
+++ src/routes.js
@@ -16,11 +16,10 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 const upload = multer(multerConfig);
 
-routes.post('/users', UserController.store);
-routes.post('/sessions', SessionController.store);
-
 routes.use(authMiddleware);
 
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);

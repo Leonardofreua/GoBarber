8e3705138c1ae7392fd4803ede2cc0d3a2c63748
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-04-23 17:51:16 -0300

    Authentication implemented.

--- frontend/src/store/modules/auth/reducer.js
+++ frontend/src/store/modules/auth/reducer.js
@@ -1,7 +1,18 @@
-const INTIAL_STATE = {};
+import produce from 'immer';
+
+const INTIAL_STATE = {
+  token: null,
+  signed: false,
+  loading: false,
+};
 
 export default function auth(state = INTIAL_STATE, action) {
   switch (action.type) {
+    case '@auth/SIGN_IN_SUCCESS':
+      return produce(state, (draft) => {
+        draft.token = action.payload.token;
+        draft.signed = true;
+      });
     default:
       return state;
   }

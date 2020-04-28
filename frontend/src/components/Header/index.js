aa39d4b0eeb39eefd607ced8af069b85502fd49f
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-04-28 19:12:27 -0300

    Stylized notifications

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import logo from '~/assets/logo-purple.svg';
+import Notifications from '~/components/Notifications';
 
+import logo from '~/assets/logo-purple.svg';
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
@@ -14,6 +15,7 @@ export default function Header() {
           <Link to="/dashboard">DASHBOARD</Link>
         </nav>
         <aside>
+          <Notifications />
           <Profile>
             <div>
               <strong>Leonardo Freua</strong>

82bac952b09019f57a3969c7bb398a2d8422742b
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-05-06 14:02:28 -0300

    Sign out implemented.

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Form, Input } from '@rocketseat/unform';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { signOut } from '~/store/modules/auth/actions';
 import { updateProfileRequest } from '~/store/modules/user/actions';
 
 import AvatarInput from './AvatarInput';
@@ -14,6 +15,10 @@ export default function Profile() {
 
   function handleSubmit(data) {
     dispatch(updateProfileRequest(data));
+  }
+
+  function handleSignOut() {
+    dispatch(signOut());
   }
 
   return (
@@ -37,7 +42,9 @@ export default function Profile() {
         <button type="submit">Update profile</button>
       </Form>
 
-      <button type="button">Sign out GoBarber</button>
+      <button type="button" onClick={handleSignOut}>
+        Sign out GoBarber
+      </button>
     </Container>
   );
 }

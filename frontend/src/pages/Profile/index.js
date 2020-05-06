238d69074ccbfbf0822d189d2712274e419358fe
leonardo.freua <leonardo.freua@cresolconfederacao.com.br>
2020-05-06 11:31:34 -0300

    Created profile page

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
+import { useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Profile() {
-  return <div />;
+  const profile = useSelector((state) => state.user.profile);
+
+  function handleSubmit() {}
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Your email" />
+
+        <hr />
+
+        <Input name="oldPassword" type="password" placeholder="Old Password" />
+        <Input name="password" type="password" placeholder="New Password" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirm Password"
+        />
+
+        <button type="submit">Update profile</button>
+      </Form>
+
+      <button type="button">Sign out GoBarber</button>
+    </Container>
+  );
 }
